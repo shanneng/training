0c5a3243f461a8c5851eb81b2dbff4d867d5d499
Torrejos <shannen.mae.torrejos@accenture.com>
2023-07-20 08:03:36 +0800

    upload activity

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import './styles/index.css';
+
 const App = () => {
-  return(
-    <>
-  <div>
-    <label> color red </label>
-    <label> color blue </label>
-  </div>
-    </>
+  return (
+    <div ClassName = "container">
+      <div>
+        <h1> Login </h1>
+        <label for = "Username"> Username </label>
+        <input type = "text" name = "Type your username" />
+        <label for = "Password"> Password </label>
+        <input type = "text" name = "Type your password" />
+        <input type = "radio" name = "Remember Me" /> Remember Me
+        <button type = "button"> LOGIN </button>
+        
+        <ul>
+          <h4>Steps</h4>
+          <li>Enter your username</li>
+          <li>
+            Enter your password
+            <ul>
+              <li> Click Remember me to save your password</li>
+            </ul>
+            </li>
+            <li>Click Login button</li>
+        </ul>
+      </div>
+    </div>
   )
   
 };
