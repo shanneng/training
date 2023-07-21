8fd858b980abfa8809acf086f87ba71b56179e49
Torrejos <shannen.mae.torrejos@accenture.com>
2023-07-21 13:14:48 +0800

    update

--- src/index.js
+++ src/index.js
@@ -11,12 +11,40 @@ const App = () => {
           <h1 className="form-header"> Login </h1>
           <p className="form-label">Username</p>
           <input className="form-input" type = "text" placeholder="Type your username" />
+          <p className="form-label">Password</p>
+          <input className="form-input" type = "text" placeholder="Type your password" />
+          <input type = "radio" name = "Remember me"/> Remember me
+          <div className = "login">
+            <button type = "button">LOGIN</button>
+          </div>
         </div>
         <div className="side-color">
-          
+        </div>
+        
+      </div>
+      
+      
+      
+      <div className="container2">
+          <ul>
+           <li> <h3> Steps </h3> </li> 
+          </ul>
+          <ul>
+            <li>Enter your username</li>
+            <li>Enter your password</li>
+          <ul>
+            <li>Click Remember me to save your password</li>
+          </ul>
+            <li>Click Login button</li>
+        </ul>
+
+        <div className="side-color2">
+
         </div>
       </div>
+    
     </div>
+
   )
   
 };
